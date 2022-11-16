9d520dac488872d08a7f804a716f3153d8822826
maureen-james <manjax97@gmail.com>
2022-11-16 21:56:31 +0300

    feat:generate todays news views

--- news/views.py
+++ news/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Article
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import NewsLetterForm
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Nairobi Tribune')
-    
+
+def news_today(request):
+    date = dt.date.today()
+    # return render(request, 'all-news/today-news.html',{'date': date})
+  
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})  
